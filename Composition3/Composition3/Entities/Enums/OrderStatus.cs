76357d66caf4d8c5e00e94dc72be02c4f79b7fbe
Alex Sandro <alex.sandro.1199@gmail.com>
2022-07-19 22:36:32 -0300

    Composition3 exercise add

---
+++ Composition3/Composition3/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Composition3.Entities.Enums
+{
+    internal enum OrderStatus : int
+    {
+        PedingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Composition3/Composition3/Program.cs
@@ -0,0 +1,46 @@
+﻿using Composition3.Entities;
+using Composition3.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace Composition3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("Enter order data");
+            Console.Write("Status: ");
+            OrderStatus status = (OrderStatus)Enum.Parse(typeof(OrderStatus), Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int count = int.Parse(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, status, client);
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Enter #{i + 1} item data:");
+                Console.Write("Product name: ");
+                string nProduct = Console.ReadLine();
+                Console.Write("Product price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                Product product = new Product(nProduct, price);
+                OrderItem item = new OrderItem(quantity, price, product);
+                order.AddItem(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine(order);
+        }
+    }
+}
